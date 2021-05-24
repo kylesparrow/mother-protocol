9769a506c1d8a2682c9b04198ffc4bf64b65b734
Kyle Sparrow <kylesparrow@protonmail.com>
2021-05-24 03:28:48 -0400

    Add styles to App script, delete stylesheet

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import styled from 'styled-components'
 import { ContractKitProvider } from '@celo-tools/use-contractkit'
+import { Container } from './components/shared/layout'
 import Header from './components/header/Header'
 import MainContent from './components/mainContent/MainContent'
 import Footer from './components/footer/Footer'
 // import useSticky from './hooks/useSticky'
 import '@celo-tools/use-contractkit/lib/styles.css'
+
+const AppContainer = styled(Container)`
+  text-align: center;
+`
 
 function App() {
   return (
@@ -13,11 +19,11 @@ function App() {
       dappDescription='Mother Protocol Celo app'
       dappUrl='https://mother-protocol.com'
     >
-      <div data-testid='app'>
+      <AppContainer data-testid='app'>
         <Header />
         <MainContent />
         <Footer />
-      </div>
+      </AppContainer>
     </ContractKitProvider>
   )
 }

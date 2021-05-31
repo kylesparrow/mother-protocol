56053aa8a380b9cfdd051563596d80fe7165991a
Kyle Sparrow <kylesparrow@protonmail.com>
2021-05-31 15:42:51 -0400

    Add contractkit wrapper to App, add react-modal dep

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,38 @@
 import React from 'react'
 import styled from 'styled-components'
-import { ContractKitProvider } from '@celo-tools/use-contractkit'
+import { ContractKitProvider, Alfajores } from '@celo-tools/use-contractkit'
+import '@celo-tools/use-contractkit/lib/styles.css'
+import ReactModal from 'react-modal'
 import { Container } from './components/shared/layout'
 import Header from './components/header/Header'
 import MainContent from './components/mainContent/MainContent'
 import Footer from './components/footer/Footer'
-// import useSticky from './hooks/useSticky'
-import '@celo-tools/use-contractkit/lib/styles.css'
 
 const AppContainer = styled(Container)`
   text-align: center;
 `
 
-function App() {
+export default function WrappedApp() {
+  React.useEffect(() => ReactModal.setAppElement('#root'))
   return (
     <ContractKitProvider
       dappName='Mother Protocol'
       dappDescription='Mother Protocol Celo app'
       dappUrl='https://mother-protocol.com'
+      networks={[Alfajores]}
     >
-      <AppContainer data-testid='app'>
-        <Header />
-        <MainContent />
-        <Footer />
-      </AppContainer>
+      <App />
     </ContractKitProvider>
   )
 }
 
-export default App
+function App() {
+  // const { connect, address } = useContractKit()
+  return (
+    <AppContainer data-testid='app'>
+      <Header />
+      <MainContent />
+      <Footer />
+    </AppContainer>
+  )
+}

ca2530a2dd3b90983f7f9182326c32f757585665
Kyle Sparrow <kylesparrow@protonmail.com>
2021-06-01 01:56:11 -0400

    Add formatting helper funcs

--- src/util/format.js
+++ src/util/format.js
@@ -1,3 +1,23 @@
-const truncateAddress = (address) => `${address.substr(0, 6)}...${address.substr(-4, 4)}`
+import BigNumber from 'bignumber.js'
 
-export default truncateAddress
+export const truncateAddress = (address) => `${address.substr(0, 6)}...${address.substr(-4, 4)}`
+
+export const formatUSD = (amount) => {
+  if (!amount) return '$0.00'
+  let formatted = amount
+  if (typeof formatted === 'string') {
+    if (formatted.substr(0, 1) === '$') {
+      formatted = formatted.substring(1)
+    }
+  }
+  formatted = `$${parseFloat(amount).toFixed(2)}`
+  return formatted
+}
+
+export const formatTokens = (amount, precision = 4) => {
+  // if (!amount) return '0.0000'
+  let formatted = amount
+
+  formatted = new BigNumber(formatted).toFixed(precision)
+  return formatted
+}

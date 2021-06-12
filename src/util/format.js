ca165a621f851879a2aa3ac4d09cf7f33e879faf
Kyle Sparrow <kylesparrow@protonmail.com>
2021-06-12 15:44:44 -0400

    Add formatPercent function

--- src/util/format.js
+++ src/util/format.js
@@ -10,12 +10,11 @@ export const formatUSD = (amount) => {
       formatted = formatted.substring(1)
     }
   }
-  formatted = `$${parseFloat(amount).toFixed(2)}`
+  formatted = `$${parseFloat(formatted).toFixed(2)}`
   return formatted
 }
 
 export const formatToken = (amount, precision = 4) => {
-  // if (!amount) return '0.0000'
   let formatted = amount
 
   formatted = new BigNumber(formatted).toFixed(precision)
@@ -26,4 +25,19 @@ export const toTitleCase = (unformatted) => {
   return unformatted.replace(/\w\S*/g, function (txt) {
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
   })
+}
+
+export const formatPercent = (percent) => {
+  if (!percent) return '0.00%'
+  let formatted = percent
+  if (typeof formatted === 'string') {
+    if (formatted.substr(formatted.length - 1) === '%') {
+      formatted = formatted.substring(0, formatted.length - 1)
+    }
+  }
+  if (parseFloat(formatted) < 1) {
+    formatted = parseFloat(formatted) * 100
+  }
+  formatted = `${parseFloat(formatted).toFixed(2)}%`
+  return formatted
 }

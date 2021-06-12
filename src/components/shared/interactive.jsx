32d0eed7c758c0d9fea522ac3c1e4b2385293c32
Kyle Sparrow <kylesparrow@protonmail.com>
2021-06-12 03:37:44 -0400

    Add elements to shared

--- src/components/shared/interactive.jsx
+++ src/components/shared/interactive.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { Row } from './layout'
+import { TABLET_PORTRAIT_UPPER_BOUND } from '../../util/constants'
 
 export const Button = styled.button`
   padding: 0.7rem 1.5rem;
@@ -25,6 +26,32 @@ export const Button = styled.button`
   &:hover {
     background: #ecd36b;
     transform: scale(1.1);
+  }
+`
+
+export const Label = styled.label`
+  display: inline-flex;
+  align-items: center;
+  justify-content: flex-start;
+  padding: 3% 0;
+  margin-bottom: 4%;
+  font-size: 0.8rem;
+  color: #333;
+`
+
+export const Input = styled.input`
+  display: flex;
+  width: 100%;
+  height: 2.5rem;
+  border: 1px solid #333;
+  background: #fff;
+  color: #000;
+  padding: 2%;
+  font-size: 1.3rem;
+  margin-bottom: 4%;
+  @media only screen and (max-width: ${TABLET_PORTRAIT_UPPER_BOUND}) {
+    width: 100%;
+    font-size: 1.8rem;
   }
 `
 
@@ -59,4 +86,36 @@ export const Title = (props) => {
 
 Title.propTypes = {
   children: PropTypes.arrayOf(PropTypes.element).isRequired,
+}
+
+export const IconStyles = {
+  fontSize: '3rem',
+  border: '3px solid #000',
+  borderRadius: '100px',
+  padding: '0.1rem 0.3rem',
+}
+
+export const ExchangeIcon = (props) => {
+  const { image, styles } = props
+  let iconStyles
+  if (styles) {
+    iconStyles = { ...styles }
+  } else {
+    iconStyles = { ...IconStyles }
+  }
+  return <img src={image} alt='exchange-icon' style={iconStyles} />
+}
+
+ExchangeIcon.propTypes = {
+  image: PropTypes.string.isRequired,
+  styles: PropTypes.objectOf(PropTypes.string),
+}
+
+ExchangeIcon.defaultProps = {
+  styles: {
+    fontSize: '3rem',
+    border: '3px solid #000',
+    borderRadius: '100px',
+    padding: '0.1rem 0.3rem',
+  },
 }

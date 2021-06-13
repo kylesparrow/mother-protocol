ff7b174869027c5413bdc24dd7b79550741d7b80
Kyle Sparrow <kylesparrow@protonmail.com>
2021-06-12 20:02:34 -0400

    Add styles to icons in Coins

--- src/components/shared/Coins.jsx
+++ src/components/shared/Coins.jsx
@@ -1,36 +1,26 @@
 import React from 'react'
 import { FaSourcetree } from 'react-icons/fa'
-import { ExchangeIcon } from './interactive'
+import { ExchangeIcon, ModalIconStyles } from './interactive'
 import celoIcon from '../../assets/CELO-color-icon.svg'
 import cUSDIcon from '../../assets/cUSD-color-icon.svg'
 import cEURIcon from '../../assets/cEUR-color-icon.svg'
-// import motherIcon from '../../assets/MOM-color-icon.svg'
 import ubeIcon from '../../assets/UBE-color-icon.svg'
+// FIXME: Add MOM icon when available
+// import motherIcon from '../../assets/MOM-color-icon.svg'
 
 export default {
   MOM: {
     name: 'Mother',
     ticker: 'MOM',
-    icon: <FaSourcetree />,
+    icon: <FaSourcetree style={ModalIconStyles} />,
   },
   UBE: {
     name: 'Ubeswap',
     ticker: 'UBE',
-    icon: (
-      <ExchangeIcon
-        image={ubeIcon}
-        style={{
-          height: '25px',
-          width: '25px',
-          fontSize: '1rem',
-          borderRadius: 0,
-          padding: '0.3rem',
-        }}
-      />
-    ),
+    icon: <ExchangeIcon image={ubeIcon} style={ModalIconStyles} />,
   },
   CELO: {
-    name: 'Celo',
+    name: 'Celo Native Asset',
     ticker: 'CELO',
     icon: <ExchangeIcon image={celoIcon} />,
   },
